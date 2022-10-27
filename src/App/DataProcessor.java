7217f8666adc2416c29a45e630a281afd30aed2b
YanisAmk <159.amk.yanis@gmail.com>
2022-10-27 22:24:15 +0100

    adding discretisation methodes to DataProcessor

--- src/App/DataProcessor.java
+++ src/App/DataProcessor.java
@@ -1,11 +1,13 @@
 package src.app;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class DataProcessor {
-    public void discretisation(int q, List<String> attributeList){
+    public void discretisation1(int q, List<String> attributeList){
         List<String> distinctValues = new ArrayList<>(new HashSet<>(attributeList));
         List<String> discrettedAttributeList = new ArrayList<>();
         int n = distinctValues.size();
@@ -26,4 +28,24 @@ public class DataProcessor {
         }
 
     }
+    public void discretisation2(int k, List<String> attributeList){
+        float min= Float.parseFloat(attributeList.get(0)) ;
+        float max=  Float.parseFloat(attributeList.get(0)) ;
+        for (String s : attributeList) {
+            float val = Float.parseFloat(s);
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        }
+        float largeurInterval = (max - min)/k;
+        for (int i = 0; i < attributeList.size(); i++) {
+            
+        }
+
+    }
+
+
 }
